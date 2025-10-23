7a354950a9e4a3a252bd2385bfd412b77d7ac814
Srikakulapu Sai Teja <saitejadivya2002@gmail.com>
2025-10-23 15:27:35 +0530

    First Commit Added Initial Project Setup

---
+++ src/main/java/com/smartleavemanagement/SmartLeaveManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smartleavemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLeaveManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLeaveManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartleavemanagement/SmartLeaveManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartleavemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLeaveManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
