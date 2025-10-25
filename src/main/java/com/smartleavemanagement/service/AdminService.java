84ccf52103a6350de3885e5fdf24b2b3f2b2de71
Srikakulapu Sai Teja <saitejadivya2002@gmail.com>
2025-10-25 13:07:20 +0530

    Added functionality for admin to add new roles

--- src/main/java/com/smartleavemanagement/service/AdminService.java
+++ src/main/java/com/smartleavemanagement/service/AdminService.java
@@ -1,12 +1,12 @@
 package com.smartleavemanagement.service;
 
 import org.springframework.http.ResponseEntity;
-
 import com.smartleavemanagement.model.Admins;
 
 public interface AdminService {
 	
 	ResponseEntity<String> registerAdmin(Admins admins);
 	ResponseEntity<?> login(String username, String password);
+	ResponseEntity<String> addNewRole(String newRole,String description);
 
 }
